3bf4202396ca39b59f9ccf72fcda7d110336412f
Hidenori Maehara <h-maehara@m3.com>
2017-09-30 22:11:07 +0900

    add validation

--- src/main/kotlin/com/maeharin/kotlindvdrental/application/restcontroller/param/params.kt
+++ src/main/kotlin/com/maeharin/kotlindvdrental/application/restcontroller/param/params.kt
@@ -1,18 +1,37 @@
 package com.maeharin.kotlindvdrental.application.restcontroller.param
 
+import org.hibernate.validator.constraints.NotBlank
 import java.math.BigDecimal
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.Size
 
 data class FilmRestParam(
     var id: Int? = null,
+
+    @get:NotBlank
+    @get:Size(min = 1, max = 50)
     val title: String,
+
+    @get:Size (min = 1, max = 500)
     val description: String?,
+
+    @get:Min(1901)
+    @get:Max(2155)
     val releaseYear: Int?,
+
     val rentalDuration: Short,
+
     val rentalRate: BigDecimal,
+
     val length: Short?,
+
     val replacementCost: BigDecimal,
+
     val languageId: Int,
+
     val actorIds: List<Int> = emptyList(),
+
     val categoryIds: List<Int> = emptyList()
 )
 
