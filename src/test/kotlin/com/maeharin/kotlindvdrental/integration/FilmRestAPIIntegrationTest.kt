c6a3f9a1931e0a3ae983f4d6abf3a41086449edb
maeharin <maeharin@gmail.com>
2017-11-12 16:38:14 +0900

    add put test

--- src/test/kotlin/com/maeharin/kotlindvdrental/integration/FilmRestAPIIntegrationTest.kt
+++ src/test/kotlin/com/maeharin/kotlindvdrental/integration/FilmRestAPIIntegrationTest.kt
@@ -2,8 +2,7 @@ package com.maeharin.kotlindvdrental.integration
 
 import org.junit.Test
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -48,6 +47,40 @@ class FilmRestAPIIntegrationTest : IntegrationTestBase() {
         val id = mockMvc.perform(postRequest)
                 .andExpect(status().isCreated())
                 .andReturn().response.contentAsString
+
+        val getRequest = get("/api/v1/films/${id}")
+
+        mockMvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.title").value("映画タイトル"))
+    }
+
+    @Test
+    fun updateFilm() {
+        val id = 1
+
+        val json = """
+            {
+              "title": "映画タイトル",
+              "description": "映画説明",
+              "releaseYear": 2017,
+              "rentalDuration": 0,
+              "rentalRate": 0,
+              "length": 0,
+              "replacementCost": 0,
+              "languageId": 1,
+              "actorIds": [1,2],
+              "categoryIds": [1,2]
+            }
+        """.trimIndent()
+
+        val putRequest = put("/api/v1/films/${id}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+
+        mockMvc.perform(putRequest)
+                .andExpect(status().isOk())
 
         val getRequest = get("/api/v1/films/${id}")
 
