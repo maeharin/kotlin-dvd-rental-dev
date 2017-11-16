d388823f059ab0e8b4c69c0666add015fa995904
Hidenori Maehara <h-maehara@m3.com>
2017-11-16 16:29:53 +0900

    typescript

--- admin-front/app/javascript/src/Home.vue
+++ admin-front/app/javascript/src/Home.vue
@@ -1,27 +1,40 @@
 <template>
 <div>
-    <el-button type="primary" @click="dialogVisible = !dialogVisible">Primary</el-button>
+   <el-button type="primary" @click="dialogVisible = !dialogVisible">Primary</el-button>
+   <el-button type="primary" @click="hello">hello</el-button>
 
-    <el-dialog
-    title="Tips"
-    :visible.sync="dialogVisible"
-    width="30%"
-    >
-    <span>This is a message</span>
-    <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
-    </span>
-    </el-dialog>
+   <el-dialog
+     title="Tips"
+     :visible.sync="dialogVisible"
+     width="30%"
+   >
+     <span>This is a message</span>
+     <span slot="footer" class="dialog-footer">
+       <el-button @click="dialogVisible = false">Cancel</el-button>
+       <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+     </span>
+   </el-dialog>
 </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
     data() {
         return {
             dialogVisible: false    
         }
+    },
+    computed: {
+        foo(): string {
+            return "foo"
+        }
+    },
+    methods: {
+        hello() {
+            console.log(this.foo)
+        }
     }
-}
+})
 </script>
--- admin-front/config/webpack/environment.js
+++ admin-front/config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.set('typescript', {
+  test: /\.ts$/,
+  loader: 'ts-loader',
+  options: {
+    appendTsSuffixTo: [/\.vue$/]
+  }
+})
+
 module.exports = environment
